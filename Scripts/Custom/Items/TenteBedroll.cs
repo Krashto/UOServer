4658450bfbe268635e4fb1ac7d380fb99d835424
vince <vince@BUREAU>
2023-03-24 13:06:51 -0400

    TENTEBEDROLL

---
+++ Scripts/Custom/Items/TenteBedroll.cs
@@ -0,0 +1,165 @@
+using System.Linq;
+using Server;
+using Server.Gumps;
+using Server.Items;
+using Server.Network;
+
+namespace BedrollTent
+{
+	public class BedrollTent : Item
+	{
+		private int m_Hue;
+		private SmallTent m_Tent;
+		private const int MAX_TENTS = 1;
+		private static int m_TentCount = 0;
+
+		[Constructable]
+		public BedrollTent() : this(0)
+		{
+		}
+
+		[Constructable]
+		public BedrollTent(int hue) : base(0xA57)
+		{
+			Name = "Bedroll";
+			Movable = true;
+			m_Hue = hue;
+			m_Tent = null;
+			Hue = hue;
+		}
+
+		public BedrollTent(Serial serial) : base(serial)
+		{
+		}
+
+		public override void OnDoubleClick(Mobile from)
+		{
+			if (from.InRange(GetWorldLocation(), 1))
+			{
+				if (from.Backpack != null && this.IsChildOf(from.Backpack))
+				{
+					if (from.HasGump(typeof(BedrollGump)))
+					{
+						from.CloseGump(typeof(BedrollGump));
+					}
+					else
+					{
+						from.SendGump(new BedrollGump(this));
+					}
+				}
+				else
+				{
+					from.SendLocalizedMessage(1042001); // That must be in your pack for you to use it.
+				}
+			}
+			else
+			{
+				from.SendLocalizedMessage(500446); // That is too far away.
+			}
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write((int)0); // version
+			writer.Write(m_Hue);
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+			int version = reader.ReadInt();
+			m_Hue = reader.ReadInt();
+		}
+
+
+		private class BedrollGump : BaseProjectMGump
+		{
+			private SmallTent m_Tent;
+			
+			private BedrollTent m_Bedroll;
+
+
+			public BedrollGump(BedrollTent bedroll) : base("Menu Tente", 100, 60, false)
+
+			{
+				m_Bedroll = bedroll;
+
+				Closable = true;
+				Disposable = true;
+				Dragable = true;
+				Resizable = false;
+				AddPage(0);
+
+				AddButton(65, 85, 4005, 4007, 1, GumpButtonType.Reply, 0);
+				AddLabel(105, 85, 0, "Ajouter une Tente");
+
+				AddButton(65, 135, 4005, 4007, 2, GumpButtonType.Reply, 0);
+				AddLabel(105, 135, 0, "Retirer une Tente");
+			}
+
+			public override void OnResponse(NetState sender, RelayInfo info)
+			{
+				Mobile from = sender.Mobile;
+
+				if (from == null || info.ButtonID == 0)
+					return;
+
+				switch (info.ButtonID)
+				{
+					case 1:
+						AddTent(from, m_Bedroll.m_Hue); // Utilisation du hue de BedrollTent courant
+						break;
+					case 2:
+						RemoveTent(from);
+						break;
+				}
+			}
+
+			private void AddTent(Mobile from, int hue)
+			{
+				if (m_Tent != null)
+				{
+					from.SendMessage("Vous ne pouvez poser qu'une seule tente à la fois.");
+					return;
+				}
+
+				if (m_TentCount >= MAX_TENTS)
+				{
+					from.SendMessage("Vous avez atteint le nombre maximum de tentes.");
+					return;
+				}
+
+
+				m_Tent = new SmallTent(); // Création d'une tente sans couleur
+				m_Tent.Hue = m_Bedroll.Hue; // Définition de la couleur de la tente avec la couleur du Bedroll
+				m_Tent.MoveToWorld(from.Location, from.Map);
+				from.SendMessage("Vous ajoutez une tente.");
+				m_TentCount++;
+			}
+
+			private void RemoveTent(Mobile from)
+			{
+				Map map = from.Map;
+				Point3D location = from.Location;
+
+				Item[] items = from.GetItemsInRange(15).ToArray();
+
+				foreach (Item item in items)
+				{
+					if (item is SmallTent)
+					{
+						item.Delete();
+						from.SendMessage("Vous retirez la tente.");
+						m_TentCount--;
+						return;
+
+					}
+
+				}
+
+				from.SendMessage("Il n'y a plus de tente.");
+			}
+		}
+	}
+}
