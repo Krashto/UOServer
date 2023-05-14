01982563028a9d6d20c249d19dc470a17b70d836
Eowymos <eowymos@msn.com>
2023-05-14 12:14:12 -0400

    Version 2.1, ajustement du système de rattrapage et du système de reset d'exp, les mettre en phase, à partir de 18h00 QC

--- Scripts/Custom/Experience/ExperienceSystem.cs
+++ Scripts/Custom/Experience/ExperienceSystem.cs
@@ -2,6 +2,7 @@
 using Server.Mobiles;
 using Server.Commands;
 using Server.Custom;
+using Server.Items;
 
 namespace Server.CustomScripts.Systems.Experience
 {
@@ -67,15 +68,13 @@ namespace Server.CustomScripts.Systems.Experience
     {
         public const int ExpGainPerTick = 200;
         public const int MaxExpAllowedByDay = 1000;
-        public int MaxExpRetard => (DateTime.Now - CustomPersistence.Ouverture).Days * 1000;
+		public int MaxExpRetard => (new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 18, 0, 0) - CustomPersistence.Ouverture).Days * 1000;
 
         [CommandProperty(AccessLevel.GameMaster)]
-        public int Exp { get; set; }
-
-        [CommandProperty(AccessLevel.GameMaster)]
-        public int Niveau { get; set; }
-
-        [CommandProperty(AccessLevel.GameMaster)]
+		public int Exp { get; set; }
+		[CommandProperty(AccessLevel.GameMaster)]
+		public int Niveau { get; set; }
+		[CommandProperty(AccessLevel.GameMaster)]
         public DateTime NextTickExp { get; set; }
 
         [CommandProperty(AccessLevel.GameMaster)]
